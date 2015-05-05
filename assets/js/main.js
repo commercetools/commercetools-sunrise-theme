86740c5e8eb80919557c164677661a7743f5c831
Julian Samarjiev <julian.samarjiev@commercetools.de>
2015-05-05 16:40:24 +0200

    get image from vertical gallery

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,16 @@
+/*****************************************************************************/
+/*
+/* POP PAGE
+/*
+/*****************************************************************************/
+
 // Close hamburger menu on click
 
-$(function () {
-	$('.navbar-collapse ul li a:not(.dropdown-toggle)').bind('click touchstart', function () {
-	    $('.navbar-toggle:visible').click();
-	});
-});
+// $(function () {
+// 	$('.navbar-collapse ul li a:not(.dropdown-toggle)').bind('click touchstart', function () {
+// 	    $('.navbar-toggle:visible').click();
+// 	});
+// });
 
 // Off-canvas menu 
 
@@ -20,3 +26,17 @@ jQuery(document).on('click', '.mega-dropdown', function(e) {
   e.stopPropagation()
 })
 
+/*****************************************************************************/
+/*
+/* PDP PAGE
+/*
+/*****************************************************************************/
+
+// Getting image from vertical thumbnail gallery on click
+
+$(document).ready(function () {
+        $("div.col-sm-12 a").click(function (event) {
+            event.preventDefault();
+            $("div.product-frame").html($("<img>").attr("src", this.href).fadeIn(1000));
+        });
+    });
