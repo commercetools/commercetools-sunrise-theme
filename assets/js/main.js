f7808ea8f7803cb33b10698570274de1cd156d29
Julian Samarjiev <julian.samarjiev@commercetools.de>
2015-05-08 10:26:07 +0200

    two handle slider added

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,6 +26,22 @@ $(document).ready(function () {
 //   e.stopPropagation();
 // });
 
+// Price range slider 
+
+$(function() {
+    $( "#slider-range" ).slider({
+      range: true,
+      min: 0,
+      max: 1000,
+      values: [ 0, 300 ],
+      slide: function( event, ui ) {
+        $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+      }
+    });
+    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+      " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+  });
+
 /*****************************************************************************/
 /*
 /* PDP PAGE
