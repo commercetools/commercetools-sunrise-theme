3824c8171ff47076a9b170e995c30bda97f7a8af
Laura Luiz <laura.luiz@commercetools.de>
2015-06-02 17:32:53 +0200

    Update structure

--- input/assets/js/main.js
+++ input/assets/js/main.js
@@ -4,28 +4,49 @@
 /*
 /*****************************************************************************/
 
-// Main navigation
+// Activate megamenu accordion on smaller screens
 
-$(document).ready(function(){
-    $(".dropdown").hover(
-        function() { $('.dropdown-menu', this).stop().fadeIn("fast");
-        },
-        function() { $('.dropdown-menu', this).stop().fadeOut("fast");
+$(function() {
+  if ($(window).width() < 768) {
+    $(".nav-accordion").accordion({
+      heightStyle: "content",
+      active: false,
+      collapsible: true
     });
+  }
 });
 
-// Close hamburger menu on click
+// Toggling plus and minus icons for mobile navigation menu
 
-$(function () {
-  $('.navbar-collapse ul li a:not(.dropdown-toggle)').bind('click touchstart', function () {
-      $('.navbar-toggle:visible').click();
+$(".dropdown-toggle").click(function() {
+    $(this).find(".mobile-plus-content").toggleClass("mobile-minus-content");
+});
+
+// Disabling bootstrap menu close on 2nd+ level items
+
+$( ".dropdown-submenu" ).click(function(event) {
+    // stop bootstrap.js to hide the parents
+    event.stopPropagation();
+    // hide the open children
+    $( this ).find(".dropdown-submenu").removeClass('open');
+    // add 'open' class to all parents with class 'dropdown-submenu'
+    $( this ).parents(".dropdown-submenu").addClass('open');
+    // this is also open (or was)
+    $( this ).toggleClass('open');
+});
+
+// Close hamburger menu on select click or outside
+
+$(function() {
+  $('.navbar-collapse ul li a:not(.dropdown-toggle)').bind('click touchstart', function() {
+    $('.navbar-toggle:visible').click();
   });
 });
 
 // Off-canvas menu 
 
-$(document).ready(function () {
-  $('[data-toggle="offcanvas"]').click(function () {
+$(document).ready(function() {
+  $('[data-toggle="offcanvas"]').click(function() {
     $('.row-offcanvas').toggleClass('active');
   });
 });
@@ -33,18 +54,18 @@ $(document).ready(function () {
 // Price range slider 
 
 $(function() {
-    $( "#slider-range" ).slider({
-      range: true,
-      min: 0,
-      max: 1000,
-      values: [ 0, 300 ],
-      slide: function( event, ui ) {
-        $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
-      }
-    });
-    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
-      " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+  $("#slider-range").slider({
+    range: true,
+    min: 0,
+    max: 1000,
+    values: [0, 300],
+    slide: function(event, ui) {
+      $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
+    }
   });
+  $("#amount").val("$" + $("#slider-range").slider("values", 0) +
+    " - $" + $("#slider-range").slider("values", 1));
+});
 
 /*****************************************************************************/
 /*
@@ -54,16 +75,74 @@ $(function() {
 
 // Getting image from vertical thumbnail on click
 
-$(document).ready(function () {
-        $(".product-thumb").click(function (event) {
-            event.preventDefault();
-            $("div.product-frame").html($("<img>").attr("src", this.href).fadeIn(100));
-        });
-    });
+$(document).ready(function() {
+  $(".product-thumb").click(function(event) {
+    event.preventDefault();
+    $("div.product-frame").html($("<img>").attr("src", this.href).fadeIn(100));
+  });
+});
 
 // Loading first child of vertical thumbnail on pageload 
 
-$(document).ready(function () {
-    var href = $('#first-thumb').get(0).href;
-    $('div.product-frame').html($('<img>').attr('src', href).fadeIn(100));
+$(document).ready(function() {
+  var href = $('#first-thumb').get(0).href;
+  $('div.product-frame').html($('<img>').attr('src', href).fadeIn(100));
 });
+
+/*****************************************************************************/
+/*
+/* CART PAGE
+/*
+/*****************************************************************************/
+
+// Quantity counter / spinner
+
+(function() {
+  window.inputNumber = function(el) {
+    var min = el.attr('min') || false;
+    var max = el.attr('max') || false;
+    var els = {};
+    els.dec = el.prev();
+    els.inc = el.next();
+    el.each(function() {
+      init($(this));
+    });
+    function init(el) {
+      els.dec.on('click', decrement);
+      els.inc.on('click', increment);
+      function decrement() {
+        var value = el[0].value;
+        value--;
+        if(!min || value >= min) {
+          el[0].value = value;
+        }
+      }
+      function increment() {
+        var value = el[0].value;
+        value++;
+        if(!max || value <= max) {
+          el[0].value = value++;
+        }
+      }
+    }
+  };
+})();
+inputNumber($('.input-number'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
