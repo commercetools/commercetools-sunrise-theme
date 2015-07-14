d144427cd7593fad13468fac9ca88167b097c367
Julian Samarjiev <julian.samarjiev@commercetools.de>
2015-07-14 15:04:58 +0200

    fix price slider bug on mobile

--- input/assets/js/main.js
+++ input/assets/js/main.js
@@ -61,17 +61,17 @@ $(document).ready(function() {
 
 // Price range slider
 $(function() {
-  $("#slider-range").slider({
+  $(".slider-range").slider({
     range: true,
     min: 0,
     max: 1000,
     values: [0, 1000],
     slide: function(event, ui) {
-      $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
+      $(".amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
     }
   });
-  $("#amount").val("$" + $("#slider-range").slider("values", 0) +
-    " - $" + $("#slider-range").slider("values", 1));
+  $(".amount").val("$" + $(".slider-range").slider("values", 0) +
+    " - $" + $(".slider-range").slider("values", 1));
 });
 
 // Adding dynamic ID to quickview modals
