50887955f634259bc554c01c307b8b1abb4312d2
Julian Samarjiev <julian.samarjiev@commercetools.de>
2015-07-17 09:57:29 +0200

    location dropdown styling and functionality to close when clicking outside of it added

--- input/assets/js/main.js
+++ input/assets/js/main.js
@@ -16,6 +16,15 @@ $(".location-dropdown-toggle").click(function () {
   $(".location-dropdown").slideToggle();
 });
 
+$(document).mouseup(function (e) {
+  var container = $(".location-dropdown");
+  if (!container.is(e.target) // if the target of the click isn't the container
+    && container.has(e.target).length === 0) // nor a descendant of the container
+  {
+    container.hide();
+  }
+});
+
 /*****************************************************************************/
 /*
 /* POP PAGE
