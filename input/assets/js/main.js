5373f4c3b083aab023abe5214de00377e01de05e
Adnan Asani <adnan.asani@commercetools.de>
2015-09-11 14:24:16 +0200

    feature/67: Allowing only toggle for now.

--- input/assets/js/main.js
+++ input/assets/js/main.js
@@ -221,48 +221,48 @@ $(document).ready(function() {
     }
   };
 })();
+
 $(function() {
-  inputNumber($('.input-number'));
-  var cartContentWrapper = $('.cart-content'),
-      cartItems = $('.single-cart-item', cartContentWrapper);
+    inputNumber($('.input-number'));
+    var cartContentWrapper = $('.cart-content'),
+        cartItems = $('.single-cart-item', cartContentWrapper);
 
-  function cartItemManager() {
-    var item = $(this),
-        editSectionForm = $('.edit-section-form', item),
-        editSectionOptions = $('.edit-section-options', item),
-        editAction = $('.edit-action', editSectionOptions),
-        selectors = $('.selector', editSectionForm),
-        targets = $('.cart-color-size > span', item);
+    function cartItemManager() {
+        var item = $(this),
+            editSectionForm = $('.edit-section-form', item),
+            editSectionOptions = $('.edit-section-options', item),
+            editAction = $('.edit-action', editSectionOptions);
+            //selectors = $('.selector', editSectionForm),
+            //targets = $('.cart-color-size > span', item);
 
-    function closeForm() {
-      editSectionForm.hide(0, function() {
-        editSectionOptions.show();
-      });
+        function closeForm() {
+            editSectionForm.hide(0, function() {
+                editSectionOptions.show();
+            });
+        }
+
+        function updateForm() {
+            //selectors.each(function(index) {
+            //    $(targets[index]).text($(this).find('option:selected').text());
+            //});
+            closeForm();
+        }
+
+        function openForm() {
+            var updateAction = $('.update-action', editSectionForm),
+                cancelAction = $('.cancel-action', editSectionForm);
+
+            editSectionOptions.hide(0, function() {
+                editSectionForm.show();
+                cancelAction.click(closeForm);
+                updateAction.click(updateForm);
+            });
+        }
+
+        // setting up the listener.
+        editAction.click(openForm);
     }
-
-    function updateForm() {
-      selectors.each(function(index) {
-        $(targets[index]).text($(this).find('option:selected').text());
-      });
-      closeForm();
-    }
-
-    function openForm() {
-      var updateAction = $('.update-action', editSectionForm),
-          cancelAction = $('.cancel-action', editSectionForm);
-
-      editSectionOptions.hide(0, function() {
-        editSectionForm.show();
-
-        cancelAction.click(closeForm);
-        updateAction.click(updateForm);
-      });
-    }
-    
-    editAction.click(openForm);
-  }
-
-  cartItems.each(cartItemManager);
+    cartItems.each(cartItemManager);
 });
 
 // jQuery UI - Tooltip on hover
