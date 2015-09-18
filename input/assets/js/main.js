b2bed6b7824fecb142702cfd3e66dd6293bc6664
Julian Samarjiev <julian.samarjiev@commercetools.de>
2015-09-18 10:06:32 +0200

    remove unnecessary JS

--- input/assets/js/main.js
+++ input/assets/js/main.js
@@ -179,16 +179,6 @@ $('.dropdown-toggle').click(function(event) {
   }
 });
 
-// Close dropdown when clicking outside of it
-// $(document).mouseup(function (e) {
-//   var container = $(".location-dropdown");
-//   if (!container.is(e.target) // if the target of the click isn't the container
-//     && container.has(e.target).length === 0) // nor a descendant of the container
-//   {
-//     container.hide();
-//   }
-// });
-
 /*****************************************************************************/
 /*
  /* HOME PAGE
