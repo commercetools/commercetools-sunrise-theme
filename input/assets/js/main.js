31d53616613f8becfddaaae877d759fd0f57ebfd
Julian Samarjiev <julian.samarjiev@commercetools.de>
2015-10-06 10:50:36 +0200

    edit button: change id selector into class selector

--- input/assets/js/main.js
+++ input/assets/js/main.js
@@ -518,12 +518,12 @@ $('.payment-text').change(function() {
 });
 
 /*
-My Account: Personal Details page 
+My Account: Personal Details page
 */
 
 // Hide/show personal details and edit section
 $('.personal-details-edit-wrapper').hide();
-$('#personal-details-edit-btn').click(function() {
+$('.personal-details-edit-toggle').click(function() {
   $('.personal-details-landing-wrapper').hide();
   $('.personal-details-edit-wrapper').show();
 })
