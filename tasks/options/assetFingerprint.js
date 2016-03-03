1a1833aafde5f43a28a16cd139072d08d4863d91
Laura Luiz <laura.luiz@commercetools.de>
2016-03-03 18:22:52 +0100

    Enable asset fingerprinting

---
+++ tasks/options/assetFingerprint.js
@@ -0,0 +1,50 @@
+module.exports = {
+
+  options: {
+    manifestPath: "output/assets.json",
+    findAndReplaceFiles: [
+      "output/assets/css/**/*.{css,map}",
+      "output/assets/js/**/*.js",
+      "output/templates/**/*.hbs",
+      "output/site/**/*.html"
+    ],
+    keepOriginalFiles: false
+  },
+
+  images: {
+    files: [
+      {
+        expand: true,
+        cwd: "output/assets",
+        src: [
+          "img/**/*",
+          "!img/favicon.ico"
+        ],
+        dest: "output/assets"
+      }
+    ]
+  },
+
+  assets: {
+    files: [
+      {
+        expand: true,
+        cwd: "output/assets",
+        src: [
+          "fonts/**/*",
+          "js/**/*.js"
+        ],
+        dest: "output/assets"
+      },
+      {
+        expand: true,
+        cwd: "output/assets/css",
+        src: [
+          "**/*.css"
+        ],
+        dest: "output/assets/css"
+      }
+    ]
+  }
+
+}
