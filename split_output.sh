085b7c1b61e926902934cd07e2c3d05131d699ec
Jens Schulze <jens.schulze@commercetools.de>
2016-03-15 14:52:53 +0100

    switch from subtree to clone for splitting output

--- split_output.sh
+++ split_output.sh
@@ -4,24 +4,23 @@ if [ "$TRAVIS_REPO_SLUG" == "sphereio/commercetools-sunrise-theme" ] && [ "$TRAV
 
     export SPLIT_REPO=https://${GH_TOKEN}@github.com/jayS-de/sunrise-theme
     export LAST_LOG_ENTRY=`git log --pretty=format:%B -1`
+
     git config --global user.email "automation@commercetools.de"
     git config --global user.name "Travis CI"
     git config --global github.token "${GH_TOKEN}"
 
-    echo -e "Adding subtree for release...\n"
-    git subtree add -q --prefix composer ${SPLIT_REPO} master --squash > /dev/null 2>&1
-
     grunt release-composer
 
-    echo -e "Adding output to subtree...\n"
+    cp -R composer ${HOME}/theme-output
 
-    git add -A
+    cd ${HOME}
+    git clone --quiet ${SPLIT_REPO} sunrise-theme > /dev/null 2>&1
+
+    cd ${HOME}/sunrise-theme
+    cp -Rf ${HOME}/theme-output/* .
+    git add -A .
+
+    git status
     git commit -m "${LAST_LOG_ENTRY}"
-
-    echo -e "Pushing output to subtree...\n"
-    git subtree push -q --prefix composer ${SPLIT_REPO} master --squash > /dev/null 2>&1
-
-    git reset --hard origin/master
-
-    echo -e "Subtree split done.\n"
+    git push origin/master > /dev/null 2>&1
 fi
