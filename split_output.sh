be798ba3325de9de3c2493627276f1a293f9fddd
Jens Schulze <jens.schulze@commercetools.de>
2016-03-15 15:20:56 +0100

    use temp directories for split

--- split_output.sh
+++ split_output.sh
@@ -10,16 +10,22 @@ if [ "$TRAVIS_REPO_SLUG" == "sphereio/commercetools-sunrise-theme" ] && [ "$TRAV
 
     grunt release-composer
 
-    cp -R composer ${HOME}/theme-output
+    OUT=$(mktemp -d ${HOME}/output.XXXXXXXXXX)
+    REPO=$(mktemp -d ${HOME}/theme.XXXXXXXXXX)
+    cp -R composer/* ${OUT}
 
     cd ${HOME}
-    git clone --quiet ${SPLIT_REPO} sunrise-theme > /dev/null 2>&1
+    git clone --quiet ${SPLIT_REPO} ${REPO} > /dev/null 2>&1
 
-    cd ${HOME}/sunrise-theme
-    cp -Rf ${HOME}/theme-output/* .
+    cd ${REPO}
+    git rm -rf .
+    cp -Rf ${OUT}/* .
     git add -A .
 
     git status
     git commit -m "${LAST_LOG_ENTRY}"
-    git push origin/master > /dev/null 2>&1
+    git push origin master > /dev/null 2>&1
+
+    rm -rf ${OUT}
+    rm -rf ${REPO}
 fi
